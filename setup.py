3c4550b0cd159564d34dd11f7677d8aa0c50db07
Petr "Stone" Hracek <phracek@redhat.com>
2021-10-25 10:40:00 +0200

    Add GitHub workflow for release to PyPi
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2018 CWT Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Authors: Petr Hracek <pkubat@redhat.com>
+
+from setuptools import setup, find_packages
 
 
 def get_requirements():
@@ -12,11 +36,24 @@ def get_requirements():
 
 setup(
     name="container-ci-suite",
+    description='A python3 container CI tool for testing images.',
     version="0.0.1",
-    packages=find_packages(exclude=["examples"]),
+    keywords='tool,containers,images,tests',
+    packages=find_packages(exclude=["tests"]),
     url="https://github.com/phracek/container-ci-suite",
     license="MIT",
     author="Petr Hracek",
     author_email="phracek@redhat.com",
     install_requires=get_requirements(),
+    scripts=[],
+    setup_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
 )
