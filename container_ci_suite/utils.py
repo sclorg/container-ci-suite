8e633521e6859b9c657a7eee356b8a3d5a3604e9
Petr "Stone" Hracek <phracek@redhat.com>
2024-10-30 08:46:28 +0100

    Allow dnsNames specific for NodeJS packages.
    Allowing the names supports building NodeJS Application
    on Shared Cluster.
    
    Bumping also version to 0.5.1
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- container_ci_suite/utils.py
+++ container_ci_suite/utils.py
@@ -284,6 +284,7 @@ def save_tenant_egress_yaml(project_name: str, rules: List[str] = []) -> str:
     if not rules:
         rules = [
             "github.com", "api.github.com", "codeload.github.com", "pypi.org", "www.cpan.org",
+            "registry.npmjs.org", "npmjs.org", "npmjs.com",
             "backpan.perl.org", "www.metacpan.org", "files.pythonhosted.org", "getcomposer.org",
         ]
     generated_yaml = []
